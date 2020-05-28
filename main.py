4c94cff6f2b2aad9130f0eb24662643b419262c3
whusober <whusober@gmail.com>
2020-05-28 18:29:24 +0800

    锁定财经专栏爬取信息

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ def get_page(url):
 if __name__ == '__main__':
     for text_id in range(9183059, 9197224):
         for date in range(13, 29):
+            # 构造URL
             url = 'http://www.chinanews.com/cj/2020/05-' + str(date) + '/' + str(text_id) + '.shtml'
             print(url)
             try:
                 page = get_page(url)
+                # 修改网页编码，防止乱码
                 page.encoding = 'utf-8'
+                # 有内容的话就解析
                 if page.status_code != 404:
                     html = page.text
                     soup = BeautifulSoup(html, 'html.parser')
                     content = soup.find('div', attrs={'class':'content'}).find_all('p')
                     for i in range(len(content)):
                         content[i] = content[i].text
+                    # 将爬取到的文本保存到本地
                     filename = 'data/' + str(text_id) +'.txt'
                     with open(filename, 'w') as f:
                         for i in content:
