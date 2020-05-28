42ee1f78ebc957dfdc6c1836cf5dada08ba2bef2
whusober <whusober@gmail.com>
2020-05-28 17:22:25 +0800

    change data source to China news

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import requests
+import random
 from bs4 import BeautifulSoup
+from UA import USER_AGENT_LIST
 
 def get_page(url):
-    return requests.get(url)
+    # 随机选取一个user-agent
+    USER_AGENT = random.choice(USER_AGENT_LIST)
+    # 构造请求头
+    headers = {'user-agent': USER_AGENT}
+    # 返回网页
+    return requests.get(url, headers=headers)
 
 if __name__ == '__main__':
-    for text_id in range(5513135, 5515514):
-        for date in range(20, 29):
-            url = 'http://www.gov.cn/xinwen/2020-05/' + str(date) + '/content_' + str(text_id) + '.htm'
+    # url = 'http://channel.chinanews.com/cns/cl/2020lhgg-wz.shtml?pager=36'
+    # page = get_page(url)
+    # page.encoding = 'utf-8'
+    # html = page.text
+    # soup = BeautifulSoup(html, 'html5lib')
+    # pass
+    for text_id in range(9183059, 9197224):
+        for date in range(13, 29):
+            url = 'http://www.chinanews.com/gn/2020/05-' + str(date) + '/' + str(text_id) + '.shtml'
             print(url)
             try:
                 page = get_page(url)
+                page.encoding = 'utf-8'
                 if page.status_code == 404:
                     break
-                html = page.content
+                html = page.text
                 soup = BeautifulSoup(html, 'html.parser')
-                content = soup.currentTag.text.split()
-                if content[0] == '中国政府网':
-                    break
+                content = soup.find('div', attrs={'class':'content'}).find_all('p')
+                for i in range(len(content)):
+                    content[i] = content[i].text
                 filename = 'data/' + str(text_id) +'.txt'
                 with open(filename, 'w') as f:
                     for i in content:
                         f.write(i)
                         f.write('\n')
-                print(content)
+                print(text_id)
             except:
                 pass
